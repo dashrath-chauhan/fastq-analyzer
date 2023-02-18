1d02856a2fb9204ef3a757681b8330afc86ca9c9
dashrath chauhan <dashrathcomp@gmail.com>
2023-02-18 17:51:16 +0530

    Task1: Count sequences.

---
+++ FastQReader.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FastQReader {
+
+    public static void main(String[] args) {
+    	
+    	if (args.length != 2) {
+            System.out.println("Usage: FastqCounter [--count-sequences | --count-nucleotides] input.fastq");
+            System.exit(1);
+        }
+
+        String flag = args[0];
+        String filename = args[1];
+        
+        int sequenceCount = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            boolean inSequence = false;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("@")) {
+                    if (!inSequence) {
+                        sequenceCount++;
+                        inSequence = true;
+                    }
+                } else if (line.startsWith("+")) {
+                    inSequence = false;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (flag.equals("--count-sequences")) {
+        	System.out.println("Number of sequences in " + filename + ": " + sequenceCount);
+        } else {
+            System.out.println("Usage: FastqCounter [--count-sequences | --count-nucleotides] input.fastq");
+            System.exit(1);
+        }
+    }
+}
